151ee4e93b60edf560a833dae783ab7e799e1e37
shashwatsinha <shashwatsinha@gmail.com>
2016-06-21 20:20:44 -0400

    Basic Game

---
+++ FMV/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+    public GameObject target;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, -10.0f);
+    }
+}
